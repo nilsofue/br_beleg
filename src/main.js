be73ab3d05ddc49c10779dba7d6d54ec7bf833e8
Nils <nilsofue@googlemail.com>
2019-11-11 10:37:26 +0100

    Babylon.js Projekt erstellt

---
+++ src/main.js
@@ -0,0 +1,58 @@
+class Main {
+  constructor(canvas) {
+    this.canvas = canvas; // Get the canvas element
+    this.engine = new BABYLON.Engine(this.canvas, true); // Generate the BABYLON 3D engine
+  }
+
+  init() {
+    var scene = this._createScene(); //Call the createScene function
+
+    // Register a render loop to repeatedly render the scene
+    this.engine.runRenderLoop(function() {
+      scene.render();
+    });
+
+    // Watch for browser/canvas resize events
+    window.addEventListener("resize", function() {
+      this.engine.resize();
+    });
+  }
+
+  _createScene() {
+    // Create the scene space
+    var scene = new BABYLON.Scene(this.engine);
+
+    // Add a camera to the scene and attach it to the canvas
+    var camera = new BABYLON.ArcRotateCamera(
+      "Camera",
+      Math.PI / 2,
+      Math.PI / 2,
+      2,
+      new BABYLON.Vector3(0, 0, 5),
+      scene
+    );
+
+    camera.attachControl(this.canvas, true);
+
+    // Add lights to the scene
+    var light1 = new BABYLON.HemisphericLight(
+      "light1",
+      new BABYLON.Vector3(1, 1, 0),
+      scene
+    );
+    var light2 = new BABYLON.PointLight(
+      "light2",
+      new BABYLON.Vector3(0, 1, -1),
+      scene
+    );
+
+    // Add and manipulate meshes in the scene
+    var sphere = BABYLON.MeshBuilder.CreateSphere(
+      "sphere",
+      { diameter: 2 },
+      scene
+    );
+
+    return scene;
+  }
+}
